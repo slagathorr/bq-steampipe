51a7b7f0db94b3c1e5af7de5e3e791ae0828d5a7
slagathorr <briansuk@gmail.com>
2022-01-25 16:54:56 -0500

    terraform hello world

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "var_gcp_project_id" {
+    description = "Google Cloud project ID"
+    type        = string
+}
+
+variable "var_gcp_region" {
+    description = "Google Cloud region"
+    type        = string
+}
