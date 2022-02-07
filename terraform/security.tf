eafb188f9a80ead94dc55a8788f2af845227f6e5
slagathorr <briansuk@gmail.com>
2022-02-07 15:58:54 -0500

    fixed SA name

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,7 +1,7 @@
 # Service account that Steampipe will use to access Google APIs
 resource "google_service_account" "steampipe_svc_account" {
   account_id   = var.var_steampipe_svc_account_name
-  display_name = "Service Account"
+  display_name = "sa - Steampipe Viewer Service"
 }
 
 resource "google_secret_manager_secret" "steampipe-sa-secret" {
@@ -13,3 +13,4 @@ resource "google_secret_manager_secret" "steampipe-sa-secret" {
 
     depends_on  = [google_project_service.secretmanager_api]
 }
+
