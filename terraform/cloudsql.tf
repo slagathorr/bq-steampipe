27c1c45eda5319b84335a898a8c3c02f24bc43d0
slagathorr <briansuk@gmail.com>
2022-02-07 17:35:16 -0500

    broke out cloud sql into its own file

---
+++ terraform/cloudsql.tf
@@ -0,0 +1,16 @@
+# CloudSQL for Postgres Passthrough
+resource "google_sql_database_instance" "steampipe_postgres" {
+  name              = "steampipe-postgres"
+  database_version  = "POSTGRES_13"
+  region            = "us-central1"
+  project           = var.var_gcp_project_id
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-custom-1-3840"
+  }
+  
+  #change me later
+  deletion_protection = false
+}
