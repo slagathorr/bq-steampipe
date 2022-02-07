923d834bceb1718b19e54dce0a25d16d9e1d09c6
slagathorr <briansuk@gmail.com>
2022-02-07 16:41:40 -0500

    added comments and cleaned up code

--- terraform/services.tf
+++ terraform/services.tf
@@ -1,13 +1,8 @@
+# This entire file includes the APIs needed to spin up the right resources.
+
 resource "google_project_service" "cloudsql_api" {
   project                       = var.var_gcp_project_id
   service                       = "sql-component.googleapis.com"
   disable_dependent_services    = true
   disable_on_destroy            = true
-}
-
-resource "google_project_service" "secretmanager_api" {
-    project                     = var.var_gcp_project_id
-    service                     = "secretmanager.googleapis.com"
-    disable_dependent_services  = true
-    disable_on_destroy          = true
 }
